fd5c2eef6d46322aff366255bc86e65fc10c9d56
noctirionvale <loukecage@gmail.com>
2026-04-05 12:48:37 +0800

    fix index command

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import './styles/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // Unregister any old service workers (Tindahan cleanup)
